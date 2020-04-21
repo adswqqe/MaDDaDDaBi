2659c0ed35ea3dd94e5c241131190d9281b812bb
adswqqe <xotjdbkdnj@naver.com>
2020-04-21 16:26:26 +0900

    UI배치 및 스크롤 뷰 진행중

---
+++ Assets/_Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    XMLManager xmlManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        xmlManager = gameObject.AddComponent<XMLManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Script/ScrollViewTestCode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollViewTestCode : MonoBehaviour
+{
+    [SerializeField]
+    GameObject contentPrefabs;
+    [SerializeField]
+    Transform _viewTr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Addelement()
+    {
+        var instance = Instantiate(contentPrefabs);
+        instance.transform.SetParent(_viewTr);
+
+    }
+}
