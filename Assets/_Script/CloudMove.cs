0a847dd306d0a50d8c4cd13bf58a2cdeb8218777
adswqqe <xotjdbkdnj@naver.com>
2020-05-26 02:16:26 +0900

    UI수정 및 마법사 애니메이션 및 프리팹 추가

---
+++ Assets/_Script/CloudMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMove : MonoBehaviour
+{
+    Vector3 pos; //현재위치
+
+    float delta = 100.0f; // 좌(우)로 이동가능한 (x)최대값
+
+    float speed = 0.01f; // 이동속도
+
+    float time; // 시간
+
+    public GameObject TextUI;
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = pos;
+
+        v.x += delta * Mathf.Sin(Time.time * speed);
+
+        // 좌우 이동의 최대치 및 반전 처리를 이렇게 한줄에 멋있게 하네요.
+
+        transform.position = v;
+
+        time += Time.deltaTime;
+        if(time > 3)
+        {
+            TextUI.SetActive(true);
+            time = 0;
+
+        }
+        else if(time > 1)
+        {
+            TextUI.SetActive(false);
+           
+        }
+
+    }
+}
